9b6d1af9003ff20d6b43eff6503fedb5393a8e9a
Vixys54 <103064740+Vixys54@users.noreply.github.com>
2025-11-28 20:41:37 -0300

    Create defaults.js

---
+++ variables/defaults.js
@@ -0,0 +1,6 @@
+module.exports = {
+  "batata": { type: "string", default: "etctha" },
+  "coins": { type: "number", default: 0 },
+  "teste": { type: "number", default: 2 },
+  "mula": { type: "string", default: "uau" },
+};
